e4aad24e15f83fc1758ad842c672b9f4ddad6f60
lucas vinicius <lucassilved@gmail.com>
2023-08-07 20:31:46 -0300

    feat: create page home template

--- lib/pages/app_page.dart
+++ lib/pages/app_page.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:timer_xadrez/pages/home_page.dart';
 
 class AppPage extends StatelessWidget {
   const AppPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
   }
 }
--- lib/pages/pages.dart
+++ lib/pages/pages.dart
@@ -1,1 +1,2 @@
 export './app_page.dart';
+export './home_page.dart';
