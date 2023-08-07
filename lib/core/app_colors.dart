d5139dd7e1d5d9e462659ff46fa9f17ca8be475f
lucas vinicius <lucassilved@gmail.com>
2023-08-07 18:46:44 -0300

    chore: structure basic and variables globals

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  const AppColors._();
+  static const Color background = Color(0xFF011F23);
+  static const Color green = Color(0xFF132E35);
+  static const Color winner = Color(0xFF3EDA44);
+  static const Color loser = Color(0xFFD33030);
+  static const Color text = Color(0xFFAFB3B7);
+}
