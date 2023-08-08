4b5db46bbdf6ebe8c65ee456032b817dca64db9b
lucas vinicius <lucassilved@gmail.com>
2023-08-08 11:21:09 -0300

    chore: modifiyng imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'pages/pages.dart';
+import 'package:timer_xadrez/pages/app_page.dart';
 
 void main() {
   runApp(const AppPage());
--- lib/pages/pages.dart
+++
@@ -1,2 +0,0 @@
-export './app_page.dart';
-export './home_page.dart';
