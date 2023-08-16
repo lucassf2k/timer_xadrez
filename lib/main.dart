bc1f3daf96ed47bbe238166c7da8ab51a878d212
srSenhor <viniciusfeliciano14@gmail.com>
2023-08-15 21:30:57 -0300

    Internacionalização do programa

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:timer_xadrez/pages/app_page.dart';
+import 'package:timer_xadrez/pages/home_page.dart';
 
 void main() {
   runApp(const AppPage());
+}
+
+class Main extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+        home: HomePage(),
+    );
+  }
 }
